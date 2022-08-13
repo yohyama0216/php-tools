383852e08d883a69178baedb230b65be47bd0c20
yohyama0216 <13836268+yohyama0216@users.noreply.github.com>
2022-08-13 21:21:37 +0900

    条件追加してみる

--- Numbers/Numbers3.php
+++ Numbers/Numbers3.php
@@ -26,6 +26,33 @@ class Numbers3 {
     }
 
     /*
+     * 連続、かつ±1の数字が出た回を表示する。
+     * 
+     * @params $times n回の連続　(3回以上は無い模様) 
+     */
+    public function getConsecutiveDifferentCharOneNumbers($times)
+    {
+        $result = [];
+        foreach($this->data as $key => $item) {
+            if ((int)$key+ $times >= count($this->data)) {
+                break;
+            }
+
+            $array = [];
+            for($i=0;$i<$times;$i++){
+                $array[] = $this->data[(int)$key+$i]['numbers'];
+            }
+
+            if ($this->getCondition($array, 'plusMinusOneToFive')) {
+                $result[] = $key;
+            } else {
+                continue ;
+            }
+        }
+        return $result;        
+    }
+
+    /*
      * 連続して同じ数字が出た回を表示する。
      * 
      * @params $times n回の連続　(3回以上は無い模様) 
@@ -43,7 +70,7 @@ class Numbers3 {
                 $array[] = $this->data[(int)$key+$i]['numbers'];
             }
 
-            if (count(array_unique($array)) == 1) {
+            if ($this->getCondition($array, 'allSame')) {
                 $result[] = $key;
             } else {
                 continue ;
@@ -51,8 +78,25 @@ class Numbers3 {
         }
         return $result;
     }
+
+    private function getCondition($array, $type)
+    {
+        if ($type == 'allSame') {
+            return (count(array_unique($array)) == 1);
+        } else if ($type == 'twoSame') {
+            return (count(array_unique($array)) == 2);
+        } else if ($type == 'leftCharSame') {
+
+        } else if ($type == 'rightCharSame') {
+
+        } else if ($type == 'middleCharSame') {
+
+        } else if ($type == 'plusMinusOneToFive') {
+            return (in_array(abs($array[1] - $array[0]),[1,5]));
+        }
+    }
 }
 
 $numbers3 = new Numbers3();
 //$numbers3->getConsecutiveHitNumbers(2);
-var_dump($numbers3->getRoundListByNumbers(111));
+var_dump($numbers3->getConsecutiveDifferentCharOneNumbers(2));
