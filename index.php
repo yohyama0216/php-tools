6f6dbbd588dac459bdab02059d196e0b7c62e406
yohyama0216 <13836268+yohyama0216@users.noreply.github.com>
2022-09-08 22:55:17 +0900

    ディレクトリ整理、gitignore追加

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 
-use Test\Numbers\NumbersPastData;
+use Test\Numbers;
+use Test\Loto\Loto;
 
 require('vendor/autoload.php');
 
 //$Converter = new Test\Converter('numbers3');
 //$html = $Converter->generateResultSQL();
 
-$NumbersPastData = new NumbersPastData(3);
-//$data = $NumbersPastData->getData();
-$Search = new Test\Search\Search($NumbersPastData);
-$Search->searchNumbersDigitPattern(10,2,'1digit');
-$Search->displayResult();
+// $NumbersPastData = new NumbersPastData(3);
+// //$data = $NumbersPastData->getData();
+// $Search = new Test\Search\Search($NumbersPastData);
+// $Search->searchNumbersDigitPattern(10,2,'1digit');
+// $Search->displayResult();
+
+$Loto = new Loto(6,100,'2020/09/01','01 02 03 04 05 06 07','11 22');
+
 
 ?>
 <html>
