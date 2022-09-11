d731d711621e06353d8cca2debb260a86537cba0
yohyama0216 <13836268+yohyama0216@users.noreply.github.com>
2022-09-11 22:14:10 +0900

    不要ファイル削除、フォルダ移動

--- cr-movie-search_old/oldStatsRoyale.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class StatsRoyale 
-{
-    private $baseUrl = "http://statsroyale.com/ja/decks/challenge-winners?type=top200&page=%s";
-    private $targetUrlList = [];
-    private $movieList = [];
-
-}
