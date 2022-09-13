d2de691b813dbf8e26d498d563ef9e2b6149a380
yohyama0216 <13836268+yohyama0216@users.noreply.github.com>
2022-09-14 08:07:45 +0900

    curlに変えてみた。少し早いかなくらい。2秒から1秒になった感じ。

--- ClashRoyale/Scraper.php
+++ ClashRoyale/Scraper.php
@@ -26,19 +26,39 @@ class Scraper
         foreach($urlList as $url) {
             $data = [];
             foreach($targetPatternList as $target => $pattern) {
+
                 $data[$target] = $this->scrape($url, $pattern);
+
             }
             $this->resultList[] = $data;
             $this->createResultJson();
         }
     }
+    private function getNow() // デバッグ
+    {
+        $datetime = new DateTime();
+        echo $datetime->format('Y-m-d H:i:s').PHP_EOL;
+    }
 
     private function scrape($url, $pattern)
     {
-        $html = file_get_contents($url);
+        $ch = curl_init(); // はじめ
+
+        //オプション
+        curl_setopt($ch, CURLOPT_URL, $url); 
+        //curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.52 Safari/537.17');
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        $html = curl_exec($ch);
+        $this->getNow();
         preg_match_all($pattern, $html, $matches);
+        $this->getNow();
+        curl_close($ch); //終了
         return $matches[0];
     }
+
 
     private function createResultJson()
     {
--- ClashRoyale/index.php
+++ ClashRoyale/index.php
@@ -2,10 +2,14 @@
 
 require('./StatsRoyaleCondition.php');
 require('./Scraper.php');
+require('../Debug/Timer.php');
+
 $start = $argv[1];
 $end = $argv[2];
 $StatsRoyaleCondition = new StatsRoyaleCondition($start,$end);
+//Timer::debug();
 $StatsRoyaleCondition->getTargetUrlList();
+//Timer::debug();
 $Scraper = new Scraper($StatsRoyaleCondition);
 $Scraper->execute();
 var_dump($Scraper->getResultList());
